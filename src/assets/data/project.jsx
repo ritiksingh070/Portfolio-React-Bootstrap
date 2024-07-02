50636e94e05e33d4ee0ff8e2c279383124e066ba
Ritik Singh <ritiksingh1809@gmail.com>
2024-07-02 23:49:34 +0530

    added projects

---
+++ src/assets/data/project.jsx
@@ -0,0 +1,61 @@
+import ElderSerene from '../images/works/ElderSerene.png'
+import SpotifyClone from '../images/works/Spotify-Clone.png'
+import BMICalculator from '../images/works/BMICalculator.png'
+import FormValidation from '../images/works/FormValidation.png'
+import GithubExplorer from '../images/works/GithubExplorer.png'
+import UnitConverter from '../images/works/UnitConverter.png'
+import TicTacToe from '../images/works/TicTacToe.png'
+
+
+export const projects = [
+  {
+    title: "Elder Care website : ElderSerene",
+    category: "Web Development",
+    imageLocation: `${ElderSerene}`,
+    websiteLiveUrl: "https://e-lder-serene-hack4-bengal-project.vercel.app/",
+    sourceCodeUrl: "https://github.com/nikku9678/ELderSerene-Hack4Bengal-Project",
+  },
+  {
+    title: "Spotiy-Clone",
+    category: "Web Development",
+    imageLocation: `${SpotifyClone}`,
+    websiteLiveUrl: "https://spotify-clone-pi-steel.vercel.app/",
+    sourceCodeUrl: "https://github.com/ritiksingh070/Spotify-Clone",
+  },
+  {
+    title: "Form Validation",
+    category: "Web Development",
+    imageLocation: `${FormValidation}`,
+    websiteLiveUrl: "https://form-validation-react-three.vercel.app/",
+    sourceCodeUrl: "https://github.com/ritiksingh070/FormValidationReact",
+  },
+  {
+    title: "Unit-Converter",
+    category: "Web Development",
+    imageLocation: `${UnitConverter}`,
+    websiteLiveUrl: "https://ritiksingh070.github.io/unit-converter/",
+    sourceCodeUrl: "https://github.com/ritiksingh070/unit-converter",
+  },
+  {
+    title: "Tic-Tac-Toe",
+    category: "Web Development",
+    imageLocation: `${TicTacToe}`,
+    websiteLiveUrl: "https://ritiksingh070.github.io/Tic-Tac-Toe-Game/",
+    sourceCodeUrl: "https://github.com/ritiksingh070/Tic-Tac-Toe-Game",
+  },
+
+  {
+    title: "BMI Calculator",
+    category: "Web Development",
+    imageLocation: `${BMICalculator}`,
+    websiteLiveUrl: "https://ritiksingh070.github.io/BMI-calculator/",
+    sourceCodeUrl: "https://github.com/ritiksingh070/BMI-calculator",
+  },
+  {
+    title: "GitHub Explorer",
+    category: "Web Development",
+    imageLocation: `${GithubExplorer}`,
+    websiteLiveUrl: "https://ritiksingh070.github.io/github_explorer//",
+    sourceCodeUrl: "https://github.com/ritiksingh070/github_explorer",
+  },
+];
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,32 +1,46 @@
-import { Image } from "react-bootstrap";
 import "../assets/css/work.css";
 
-import bootstrap_website_theme_naruto from "../assets/images/works/bootstrap_website_theme_naruto.png";
-import github_scraper_pure_selenium from "../assets/images/works/github_scraper_pure_selenium.png";
-import news_website from "../assets/images/works/news_website.png";
-import parth_desai_website from "../assets/images/works/parth_desai_website.jpg";
-import portfolio_website from "../assets/images/works/portfolio_website.png";
-import todo_and_calculator_javascript_project from "../assets/images/works/todo_and_calculator_javascript_project.jpg";
+import { projects } from "../assets/data/project";
+
+import { Image } from "react-bootstrap";
 
 export default function Work() {
-    return (
-        <section>
-            <div className="header d-flex flex-row align-items-center">
-                <div className="header-title">Work</div>
-                <div className="header-line"></div>
-            </div>
+  const visitLink = (link) => {
+    window.open(`${link}`, "_blank", "noopener,noreferrer");
+  };
+ 
 
-            <div className="works-wraper d-flex align-content-start justify-content-center flex-wrap">
-                {/* <div className="work-box">
-                    <Image src={} />
-                    <div className="work-details">
-                        <div className="category">Web Development</div>
-                        <div className="title">Netflix Clone</div>
-                    </div>
-                </div> */}
-               
-            </div>
+  return (
+    <section>
+      <div className="header d-flex flex-row align-items-center">
+        <div className="header-title">Work</div>
+        <div className="header-line"></div>
+      </div>
 
-        </section>
-    );
+      <div className="works-wraper d-flex align-content-start justify-content-center flex-wrap">
+        {projects.map((project, index) => (
+          <div className="work-box" key={index}>
+            <Image src={project.imageLocation} alt="not found" />
+            <div className="work-details">
+              <div className="category">{project.category}</div>
+              <div className="title">{project.title}</div>
+              <div className="liveCodeBtn">
+                <div>
+                  <button onClick={() => visitLink(`${project.websiteLiveUrl}`)}>
+                    Live Now
+                  </button>
+                </div>
+
+                <div>
+                  <button onClick={() => visitLink(`${project.sourceCodeUrl}`)}>
+                    Source Code
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
